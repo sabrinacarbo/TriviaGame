f98da504f16bfb8eb6e5510122d029ce429e3dad
Sabrina Carbo <sabrinacarbo@Sabrinas-MacBook-Pro.local>
2018-06-29 09:26:17 -0400

    pseudo-coding completed

---
+++ assets/javascript/main.js
@@ -0,0 +1,55 @@
+//=========================== GLOBAL VARIABLES ========================
+
+//============================= FUNCTIONS ==============================
+
+//----------------------------- GAME START ----------------------------
+
+//========================== CONDITIONALS ===========================
+
+
+//Page loads
+
+//User pushes the start button to begin the game
+
+//Timer starts to count down
+
+
+//Random generator needed:
+    //Random question is generated
+
+    //User chooses either True or False
+
+    //Next question populates randomly
+
+    //User chooses either True or False
+
+//User choice logic:
+    //If keyPressed == true && answer == true, then correctAnswerCounter ++ 
+    //and the next question is generated
+
+    //If keyPress == true && answer == false, then incorrectAnswerCounter ++
+    //and the next question is generated
+
+    //If keyPress == false && answer == false, then correctAnswerCounter ++
+    //and the next question is generated
+
+    //If keyPres == false && answer == true, then incorrectAnswerCounter ++
+    //and the next question is generated
+
+//Game completion:
+    //If questionArray < 1, then game is over
+    //If timer == 0, then game is over
+
+//Upon completion:
+    //correctAnswerCounter = (total correct answers)
+    //incorrectAnswerCounter = (total incorrect answers)
+    //unansweredCounter  = (total unanswered questions)
+
+
+
+//Option button to choose answer for question
+//// Check #x
+// $( "#x" ).prop( "checked", true );
+ 
+// // Uncheck #x
+// $( "#x" ).prop( "checked", false );
