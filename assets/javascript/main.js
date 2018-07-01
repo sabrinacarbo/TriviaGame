4c7c1e0f26363b6f2e99cef7b78b5df3ff5aaf7f
Sabrina Carbo <sabrinacarbo@sabrinas-mbp.attlocal.net>
2018-06-30 21:06:14 -0400

    random question generator works, T/F buttons are set for click event and if/else statements

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,38 +4,40 @@ var correctAnswerCounter = 0;
 var incorrectAnswerCounter = 0;
 var unansweredAnswerCounter = 0;
 
+var trueButton = true;
+var falseButton = false;
+
 var questionArray = [{
     question: "The man in black is Westley.",
     answer: true,
-},{
+}, {
     question: "Westley was the next door neighbor to Buttercup.",
     answer: false,
-},{
+}, {
     question: "Humperdinck is the Prince of Florin.",
     answer: true,
-},{
+}, {
     question: "R.O.U.S. stands for Rat of Unusual Shape.",
     answer: false,
-},{
+}, {
     question: "Buttercup nearly suffocated in the Fire Swamp.",
     answer: true,
-},{
+}, {
     question: "Domingo Montoya is the six-fingered man.",
     answer: false,
-},{
+}, {
     question: "The Dread Pirate Roberts is believed to have killed Westley while sailing to America.",
     answer: true,
-},{
+}, {
     question: "Westley poisoned Inigo.",
     answer: false,
-},{
+}, {
     question: "Inigo wanted to kill Count Rugen to avenge his father's death.",
     answer: true,
-},{
+}, {
     question: "Fezzik was made fun being the size of a teenager at the age of six when he was in school.",
     answer: false,
-}
-];
+}];
 
 console.log(questionArray[0].question);
 console.log(questionArray[0].answer);
@@ -46,22 +48,24 @@ var randomQuestion = [];
 //============================= FUNCTIONS ==============================
 
 //Page loads
-$(document).ready(function(){
+$(document).ready(function () {
 
-//----------------------------- GAME START ----------------------------
+    //----------------------------- GAME START ----------------------------
 
-function pageLoads (resetGame){
+    function pageLoads(resetGame) {
 
-    //Resets all counters
-    correctAnswerCounter = 0;
-    incorrectAnswerCounter = 0;
-    unansweredAnswerCounter = 0;
+        //Resets all counters
+        correctAnswerCounter = 0;
+        incorrectAnswerCounter = 0;
+        unansweredAnswerCounter = 0;
 
-};
+        $("#triviaQuestion").empty();
 
-//User pushes the start button to begin the game
-    $("#startBtn").click(function(startGame){
-        alert("Ready!");
+    };
+
+    //User pushes the start button to begin the game
+    $("#startBtn").click(function (startGame) {
+        // alert("Ready!");
 
         //Timer starts to count down ******* CHECK CODE ******  <================================ *****
         // Change the "display" id to "00:00."
@@ -70,66 +74,91 @@ function pageLoads (resetGame){
 
 
 
-//========================== CONDITIONALS ===========================
-function answerChoice(){
+    //========================== CONDITIONALS ===========================
+    function answerChoice() {
 
-//Loop for questions displayed
-    for (var i = 0; i < questionArray.length;i++){
+        //Loop for current question displayed
+        for (var i = 0; i < questionArray.length; i++) {
 
-    //Random question is generated
-    randomQuestion = Math.floor(Math.random(questionArray[i]));
+            //Random question is generated
+            randomQuestion = questionArray[Math.floor(Math.random() * questionArray.length)];
 
-    console.log(randomQuestion);
+            //Adds random question to HTML
+           $("#triviaQuestion").text(randomQuestion.question);
 
-    //User chooses either True or False
+        }
 
-    //Next question populates randomly
+        //User selects the TRUE button
+        $("#trueBtn").on("click", function () {
 
-    //User chooses either True or False
+            trueButton = $("#trueBtn");
+            // alert("I am true!");
 
-    // $("#trueBtn").prop("checked", true);  //May not need true or false after checked ****
-    // $("#falseBtn").prop("checked", false);
+            if ((trueButton == randomQuestion.answer && falseButton != randomQuestion.answer) || (falseButton == randomQuestion.answer && trueButton != randomQuestion.answer)){
+                correctAnswerCounter++
+            }else{
+                incorrectAnswerCounter++
+            }
 
-    }
-    
-//User choice logic:
-    //If keyPressed == true && answer == true, then correctAnswerCounter ++ 
-    //and the next question is generated
+            console.log(correctAnswerCounter++);
+            console.log(incorrectAnswerCounter++);
+        });
 
-    //If keyPress == true && answer == false, then incorrectAnswerCounter ++
-    //and the next question is generated
+        //User selects the TRUE button
+        $("#falseBtn").on("click", function () {
 
-    //If keyPress == false && answer == false, then correctAnswerCounter ++
-    //and the next question is generated
+            falseButton = $("#falseBtn");
+            // alert("I am false!");
 
-    //If keyPress == false && answer == true, then incorrectAnswerCounter ++
-    //and the next question is generated
-};
- answerChoice();
+            if ((trueButton == randomQuestion.answer && falseButton != randomQuestion.answer) || (falseButton == randomQuestion.answer && trueButton != randomQuestion.answer)){
+                correctAnswerCounter++
+            }else{
+                incorrectAnswerCounter++
+            }
+            console.log(correctAnswerCounter++);
+            console.log(incorrectAnswerCounter++);
+        });
+
+
+        //User choice logic:
+            //If button chosen matches the correct answer, then correctAnswerCounter ++, 
+            //otherwise, incorrectAnswerCounter++
+
+            // if ((trueButton == randomQuestion.answer && falseButton != randomQuestion.answer) || (falseButton == randomQuestion.answer && trueButton != randomQuestion.answer)){
+            //     correctAnswerCounter++
+            // }else{
+            //     incorrectAnswerCounter++
+            // }
+            // //Then next question is generated
+            // answerChoice();
+
+    };
+    pageLoads();
+    answerChoice();
 
 
 
 
-//Game completion:
+    //Game completion:
     //If questionArray < 1, then game is over
     //If timer == 0, then game is over
 
-//Upon completion:
+    //Upon completion:
     //correctAnswerCounter = (total correct answers)
     //incorrectAnswerCounter = (total incorrect answers)
     //unansweredAnswerCounter  = (total unanswered questions)
 
 
-//Listening event
+    //Listening event
 
 
 
-//Option button to choose answer for question
-//// Check #x
-// $( "#x" ).prop( "checked", true );
- 
-// // Uncheck #x
-// $( "#x" ).prop( "checked", false );
+    //Option button to choose answer for question
+    //// Check #x
+    // $( "#x" ).prop( "checked", true );
+
+    // // Uncheck #x
+    // $( "#x" ).prop( "checked", false );
 
 
 
